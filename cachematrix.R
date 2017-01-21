4002eb4149440bcc98ba430f3a88babddee36c27
test <hpantioukh@axcient.com>
2017-01-21 20:04:18 +0100

    Completed task

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## "makeCacheMatrix" is the special object, which can 
+## store matrix and inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setcache <- function(cache) m <<- cache
+    getcache <- function() m
+    list(set = set, get = get, 
+         setcache = setcache,
+         getcache = getcache)
 }
 
 
-## Write a short comment describing this function
+## "cacheSolve" function returns inverse matrix
+## of x. First of all it checks is there precalculated
+## inversed matrix and returns cache if exists
+## In other case it calculate inversed matrix, caches
+## and returns
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getcache()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    m <- solve(x$get())
+    x$setcache(m)
+    m
 }
